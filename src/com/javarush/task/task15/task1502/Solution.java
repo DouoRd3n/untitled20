50176ce3a4074b0f3e4effedce83bbc1017b2ad9
DouORd3n <DouORd3n@gmail.com>
2021-01-11 20:27:49 +0200

    new

--- src/com/javarush/task/task15/task1502/Solution.java
+++ src/com/javarush/task/task15/task1502/Solution.java
@@ -8,6 +8,9 @@ public class Solution {
     //добавьте классы Goose и Dragon тут
 
     public static void main(String[] args) {
+        Goose goose = new Goose();
+
+        System.out.println(goose.getSize());
 
     }
 
@@ -20,6 +23,18 @@ public class Solution {
     public static class SmallAnimal {
         String getSize() {
             return "как кошка";
+        }
+    }
+    public static class Goose extends SmallAnimal{
+        @Override
+        public String getSize() {
+            return "Гусь маленький " + super.getSize();
+        }
+    }
+    public static class Dragon extends BigAnimal{
+        @Override
+        public String getSize() {
+            return "Дракон большой" + super.getSize();
         }
     }
 }
--- src/com/javarush/task/task15/task1504/Solution.java
+++ src/com/javarush/task/task15/task1504/Solution.java
@@ -7,37 +7,81 @@ import java.util.List;
 ООП - книги
 */
 
-//public class Solution {
-//    public static void main(String[] args) {
-//        List<Book> books = new LinkedList<Book>();
-//        books.add(new MarkTwainBook("Tom Sawyer"));
-//        books.add(new AgathaChristieBook("Hercule Poirot"));
-//        System.out.println(books);
-//    }
-//
-//    abstract static class Book {
-//        private String author;
-//
-//        public Book(String author) {
-//            this.author = author;
-//        }
-//
-//        public abstract Book getBook();
-//
-//        public abstract String getTitle();
-//
-//        private String getOutputByBookType() {
-//            String agathaChristieOutput = author + ": " + getBook().getTitle() + " is a detective";
-//            String markTwainOutput = getBook().getTitle() + " was written by " + author;
-//
-//            String output = "output";
-//            //Add your code here
-//
-//            return output;
-//        }
-//
-//        public String toString() {
-//            return getOutputByBookType();
-//        }
-//    }
-//}
+public class Solution {
+    public static void main(String[] args) {
+        List<Book> books = new LinkedList<Book>();
+        books.add(new MarkTwainBook("Tom Sawyer"));
+        books.add(new AgathaChristieBook("Hercule Poirot"));
+        System.out.println(books);
+    }
+
+    abstract static class Book {
+        private String author;
+
+        public Book(String author) {
+            this.author = author;
+        }
+
+        public abstract Book getBook();
+
+        public abstract String getTitle();
+
+        private String getOutputByBookType() {
+
+            String agathaChristieOutput = author + ": " + getBook().getTitle() + " is a detective";
+            String markTwainOutput = getBook().getTitle() + " was written by " + author;
+
+            String output = "output";
+            //Add your code here
+            if (author.equals("Mark Twain")) {
+                output = markTwainOutput;
+            } else if (author.equals("Agata Christie")) {
+                output = agathaChristieOutput;
+            }
+            return output;
+        }
+
+        public String toString() {
+            return getOutputByBookType();
+        }
+    }
+
+    public static class MarkTwainBook extends Book {
+        private String title;
+
+
+        public MarkTwainBook(String title) {
+            super("Mark Twain");
+            this.title = title;
+        }
+
+        @Override
+        public MarkTwainBook getBook() {
+            return this;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+    }
+
+    private static class AgathaChristieBook extends Book {
+        private String title;
+
+        public AgathaChristieBook(String title) {
+            super("Agata Christie");
+            this.title = title;
+        }
+
+        @Override
+        public AgathaChristieBook getBook() {
+            return this;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+    }
+}
